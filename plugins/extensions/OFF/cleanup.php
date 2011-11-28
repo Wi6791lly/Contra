bc1736c9b4ae82d991b673ddfac8d8bced53bc84
Ben Alter <ben.alter@yahoo.com>
2011-11-28 00:20:29 -0500

    Updated cleanup command.

--- plugins/extensions/OFF/cleanup.php
+++ plugins/extensions/OFF/cleanup.php
@@ -1,5 +1,16 @@
 <?php
-class cleanup extends extension {
+class Cleanup extends extension {
+	/*
+	*	Cleanup module.
+	*	Made in Contra v5.4
+	*	Created by lwln
+	*	
+	*	This is a simple module for administrators of chatrooms to help them keep things organized.
+	*	The code is very sloppy, and needs work, but it does what needs to be done.
+	*	Your bot needs full administrator access to the room in order for this module to work correctly. 
+	*	Otherwise, you shouldn't be running this module.
+	*/
+
 	public $name = 'Cleanup';
 	public $version = 1;
 	public $about = 'Cleans up banned users from a room.';
@@ -9,6 +20,7 @@ class cleanup extends extension {
 	
 	protected $room;
 	protected $person;
+	protected $cleaned;
 	
 	function init() {
 		$this->addCmd('cleanup', 'c_cleanup', 100);
@@ -28,8 +40,18 @@ class cleanup extends extension {
 		foreach($users[0] as $user) {
 			$this->dAmn->unban($this->room, substr($user, 1));
 		}
-		$this->dAmn->say($this->room, '<abbr title="'.$this->person.'"></abbr>Cleaned #'.substr($this->room, 5).' <sub>['.$count.' users found]</sub>');
-		$this->unhook('e_cleanup');
+		$this->cleaned = $count;
+		$this->unhook('e_cleanup', 'recv_admin_show');
+		$this->hook('e_post', 'recv_admin_show');
+		$this->dAmn->admin($this->room, 'show users');
+	}
+	
+	function e_post() {
+		$users = $GLOBALS['crap'];
+		$list = explode(" ", $users);
+		$count = preg_match_all("/(\!)([\w'-])*/im", $users, $users);
+		$this->dAmn->say($this->room, '<abbr title="'.$this->person.'"></abbr>Cleaned #'.substr($this->room, 5).' <sub>['.$this->cleaned.' users found, '.($this->cleaned - $count).' removed]</sub>');
+		$this->unhook('e_post', 'recv_admin_show');
 	}
 }
 new cleanup($core);
